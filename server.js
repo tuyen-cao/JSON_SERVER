cf9df0b0c1735797606c0e002f14e9e67c311bbd
HARVEYNASH\tuyencaok <caokimtuyen1722@gmail.com>
2023-03-10 22:19:41 +0700

    123

--- server.js
+++ server.js
@@ -6,9 +6,17 @@ const middlewares = jsonServer.defaults();
 const auth = require('json-server-auth');
 
 
+const rules = auth.rewriter({
+  // Permission rules
+  users: 222,
+  messages: 640,
+  // Other rules
+  '/posts/:category': '/posts?category=:category',
+})
 server.db = router.db;
 server.use(middlewares);
 server.use(auth);
+server.use(rules)
 server.use(router);
 
 server.listen(3500, () => {
